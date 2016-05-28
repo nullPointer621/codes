f313077a5567b75d4bbe04524a52f19f192220b9
noor <mahnoorsaeed025@gmail.com>
2016-05-28 15:51:01 +0500

    2nd release

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+#def post_list(request):
+ #   return render(request, 'post_list.html', {})
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})
+
+#    return render(request, 'blog/post_list.html', {})
